5d66a94fdad4a488060752b912079b2dce8d3005
Ryan Haniff <haniffryan.hr@gmail.com>
2020-12-25 20:22:23 -0500

    Added image icon to frame

--- src/Test.java
+++ src/Test.java
@@ -4,12 +4,16 @@ import java.awt.*;
 public class Test extends JFrame {
 
     JFrame frame = new JFrame("First Title");
+    ImageIcon image = new ImageIcon("C:\\Users\\RyanPC\\OneDrive\\Documents\\JavaProjects\\Chess\\src\\KingSymbol.png"); //added image icon
 
     public Test () {
         frame.setVisible(true);
         frame.setSize(500, 500);
         frame.getContentPane().setBackground(new Color(255, 0, 255));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the application when you hit X
+
+
+        frame.setIconImage(image.getImage());
 
     }
 }
